8abb7f1bf60a891faeeed838d6e6305803a0a227
Juan Garay <juangaray93@gmail.com>
2020-09-06 00:54:34 -0300

    Created rule to see the use of proptypes in TS as an error

---
+++ tests/lib/rules/no-proptypes-in-ts.test.js
@@ -0,0 +1,39 @@
+// we import the check-before-type-validation ESLint rule
+const rules = require("../../../lib/rules/no-proptypes-in-ts");
+
+const RuleTester = require("eslint").RuleTester;
+
+const ruleTester = new RuleTester();
+
+// Here we pass the 'unexpected' messageId since it is the error we expect to be reported by the rule
+const errors = [{ messageId: "unexpected" }];
+
+const typeCheckRule = rules["no-proptypes-in-ts"];
+
+// Our test run with all the different test cases
+ruleTester.run("type-check", typeCheckRule, {
+  valid: [
+    {
+      filename: "Square.tsx",
+      code: "Square.height = 3;",
+      errors,
+    },
+    {
+      filename: "Square.ts",
+      code: "// There are no proptypes here!",
+      errors,
+    },
+    {
+      filename: "Square.jsx",
+      code: "Square.propTypes = { a: PropTypes.string }; // in jsx",
+      errors,
+    },
+  ],
+  invalid: [
+    {
+      filename: "Square.tsx",
+      code: "Square.propTypes = { a: PropTypes.string }; // in tsx",
+      errors,
+    },
+  ],
+});
